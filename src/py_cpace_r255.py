d8420701104c091f3a3cffdad2070f9bf7c309be
stsch9 <st.schaubeck@web.de>
2024-11-28 17:52:27 +0100

    update checks

--- src/py_cpace_r255.py
+++ src/py_cpace_r255.py
@@ -1,7 +1,11 @@
-from pysodium import crypto_hash_sha512, crypto_core_ristretto255_from_hash, crypto_core_ristretto255_scalar_random
+from symtable import Class
+
+from pysodium import (crypto_hash_sha512, crypto_core_ristretto255_from_hash, crypto_core_ristretto255_scalar_random,
+                      crypto_scalarmult_ristretto255, crypto_core_ristretto255_is_valid_point)
 
 DSI = b'CPaceRistretto255'
 s_in_bytes = 128
+identity = '0000000000000000000000000000000000000000000000000000000000000000'
 
 def prepend_len(data: bytes) -> bytes:
     """prepend LEB128 encoding of length"""
@@ -32,3 +36,38 @@ def generator_string(PRS: bytes, CI: bytes, sid: bytes):
 def calculate_generator(PRS: bytes ,CI: bytes, sid: bytes):
     gen_str = generator_string(PRS, CI, sid)
     return crypto_core_ristretto255_from_hash(crypto_hash_sha512(gen_str))
+
+# https://doc.libsodium.org/advanced/point-arithmetic/ristretto#scalar-multiplication
+def scalar_mult_vfy(s: bytes, e: bytes) -> bytes:
+    if crypto_core_ristretto255_is_valid_point(e):
+        try:
+            r = crypto_scalarmult_ristretto255(s,e)
+            return r
+        except ValueError:
+            raise ValueError("No valid result")
+    else:
+        raise ValueError('No valid group element')
+
+class CPace(object):
+    def __init__(self, PRF: bytes, ADab = b'', CI = b'', sid = b''):
+        self._prf = PRF
+        self.ADab = ADab
+        self.CI = CI
+        self.sid = sid
+        self._g = b''
+
+    def compute_Ya(self):
+        self._g = calculate_generator(self._prf, self.CI, self.sid)
+        ya = crypto_core_ristretto255_scalar_random()
+        ya = bytes.fromhex('da3d23700a9e5699258aef94dc060dfda5ebb61f02a5ea77fad53f4ff0976d08')
+        Ya = crypto_scalarmult_ristretto255(ya, self._g)
+
+        return Ya, self.ADab
+
+    def compute_Yb(self):
+        self._g = calculate_generator(self._prf, self.CI, self.sid)
+        yb = crypto_core_ristretto255_scalar_random()
+        Yb = crypto_scalarmult_ristretto255(yb, self._g)
+
+
+
--- test/test_py_cpace_r255.py
+++ test/test_py_cpace_r255.py
@@ -1,15 +1,20 @@
 import unittest
-from src.py_cpace_r255 import generator_string, calculate_generator
+from src.py_cpace_r255 import generator_string, calculate_generator, CPace, scalar_mult_vfy
+from pysodium import crypto_core_ristretto255_is_valid_point
 
 PRS = b'Password'
 CI = '6f630b425f726573706f6e6465720b415f696e69746961746f72'
 sid = '7e4b4791d6a8ef019b936c79fb7f2c57'
 gen_str = '11435061636552697374726574746f3235350850617373776f726464000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001a6f630b425f726573706f6e6465720b415f696e69746961746f72107e4b4791d6a8ef019b936c79fb7f2c57'
 generator = 'a6fc82c3b8968fbb2e06fee81ca858586dea50d248f0c7ca6a18b0902a30b36b'
-
 ADa = b'ADa'
 ya = 'da3d23700a9e5699258aef94dc060dfda5ebb61f02a5ea77fad53f4ff0976d08'
 Ya = 'd40fb265a7abeaee7939d91a585fe59f7053f982c296ec413c624c669308f87a'
+
+
+s = '7cd0e075fa7955ba52c02759a6c90dbbfc10e6d40aea8d283e407d88cf538a05'
+Y_i1 = '2b3c6b8c4f3800e7aef6864025b4ed79bd599117e427c41bd47d93d654b4a51c'
+Y_i2 = '0000000000000000000000000000000000000000000000000000000000000000'
 
 class TestCpaceR255(unittest.TestCase):
     def test_generator(self):
@@ -19,6 +24,18 @@ class TestCpaceR255(unittest.TestCase):
     def test_calculate_generator(self):
         g = calculate_generator(PRS, bytes.fromhex(CI), bytes.fromhex(sid))
         self.assertEqual(g, bytes.fromhex(generator))
+
+    def test_compute_Ya(self):
+        cpace = CPace(PRS, ADa=ADa, CI=bytes.fromhex(CI), sid=bytes.fromhex(sid))
+        Ya_star, ADa_star = cpace.compute_Ya()
+        self.assertEqual(Ya_star, bytes.fromhex(Ya))
+        self.assertEqual(ADa_star, ADa)
+
+    def test_scalar_mult_vfy(self):
+        with self.assertRaises(ValueError):
+            scalar_mult_vfy(bytes.fromhex(s), bytes.fromhex(Y_i1))
+        with self.assertRaises(ValueError):
+            scalar_mult_vfy(bytes.fromhex(s), bytes.fromhex(Y_i2))
 
 if __name__ == '__main__':
     unittest.main()
