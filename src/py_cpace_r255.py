001da4af86779883db621d42c35caeed239fbe50
stsch9 <st.schaubeck@web.de>
2024-11-28 21:51:35 +0100

    update

--- src/py_cpace_r255.py
+++ src/py_cpace_r255.py
@@ -1,5 +1,5 @@
 from symtable import Class
-
+from typing import Literal
 from pysodium import (crypto_hash_sha512, crypto_core_ristretto255_from_hash, crypto_core_ristretto255_scalar_random,
                       crypto_scalarmult_ristretto255, crypto_core_ristretto255_is_valid_point)
 
@@ -28,6 +28,30 @@ def lv_cat(*args) -> bytes:
         result += prepend_len(arg)
     return result
 
+def lexiographically_larger(bytes1: bytes, bytes2: bytes) -> bool:
+    "Returns True if bytes1>bytes2 using lexiographical ordering."
+    min_len = min(len(bytes1), len(bytes2))
+    for m in range(min_len):
+        if bytes1[m] > bytes2[m]:
+            return True
+        elif bytes1[m] < bytes2[m]:
+            return False
+    return len(bytes1) > len(bytes2)
+
+def o_cat(bytes1: bytes, bytes2: bytes) -> bytes:
+    if lexiographically_larger(bytes1, bytes2):
+        return b"oc" + bytes1 + bytes2
+    else:
+        return b"oc" + bytes2 + bytes1
+
+def transcript_oc(Ya: bytes, ADa: bytes,Yb: bytes, ADb: bytes) -> bytes:
+    result = o_cat(lv_cat(Ya, ADa),lv_cat(Yb, ADb))
+    return result
+
+def transcript_ir(Ya: bytes, ADa: bytes, Yb: bytes, ADb: bytes) -> bytes:
+    result = lv_cat(Ya, ADa) + lv_cat(Yb, ADb)
+    return result
+
 def generator_string(PRS: bytes, CI: bytes, sid: bytes):
     len_zpad = max(0, s_in_bytes - 1 - len(prepend_len(PRS)) - len(prepend_len(DSI)))
 
@@ -49,12 +73,19 @@ def scalar_mult_vfy(s: bytes, e: bytes) -> bytes:
         raise ValueError('No valid group element')
 
 class CPace(object):
-    def __init__(self, PRF: bytes, ADab = b'', CI = b'', sid = b''):
+    def __init__(self, PRF: bytes, mo: Literal['oc', 'ir'], ADa = b'', ADb = b'', CI = b'', sid = b''):
         self._prf = PRF
-        self.ADab = ADab
+        self.ADa = ADa
+        self.ADb = ADb
         self.CI = CI
         self.sid = sid
         self._g = b''
+        if mo == 'oc':
+            self.transcript = transcript_oc
+        elif mo == 'ir':
+            self.transcript = transcript_ir
+        else:
+            raise ValueError('Invalid Input')
 
     def compute_Ya(self):
         self._g = calculate_generator(self._prf, self.CI, self.sid)
@@ -62,12 +93,13 @@ class CPace(object):
         ya = bytes.fromhex('da3d23700a9e5699258aef94dc060dfda5ebb61f02a5ea77fad53f4ff0976d08')
         Ya = crypto_scalarmult_ristretto255(ya, self._g)
 
-        return Ya, self.ADab
+        return Ya, self.ADa
 
-    def compute_Yb(self):
+    def compute_Yb(self, Ya: bytes):
         self._g = calculate_generator(self._prf, self.CI, self.sid)
         yb = crypto_core_ristretto255_scalar_random()
         Yb = crypto_scalarmult_ristretto255(yb, self._g)
 
+        K = scalar_mult_vfy(yb, Ya)
 
-
+        ISK = crypto_hash_sha512(lv_cat(DSI + b"_ISK", self.sid, K) + self.transcript(Ya, self.ADa, Yb, self.ADb))
--- test/test_py_cpace_r255.py
+++ test/test_py_cpace_r255.py
@@ -1,6 +1,6 @@
 import unittest
-from src.py_cpace_r255 import generator_string, calculate_generator, CPace, scalar_mult_vfy
-from pysodium import crypto_core_ristretto255_is_valid_point
+from src.py_cpace_r255 import (generator_string, calculate_generator, CPace, scalar_mult_vfy, lexiographically_larger,
+                               transcript_oc, o_cat, transcript_ir,scalar_mult_vfy)
 
 PRS = b'Password'
 CI = '6f630b425f726573706f6e6465720b415f696e69746961746f72'
@@ -10,7 +10,9 @@ generator = 'a6fc82c3b8968fbb2e06fee81ca858586dea50d248f0c7ca6a18b0902a30b36b'
 ADa = b'ADa'
 ya = 'da3d23700a9e5699258aef94dc060dfda5ebb61f02a5ea77fad53f4ff0976d08'
 Ya = 'd40fb265a7abeaee7939d91a585fe59f7053f982c296ec413c624c669308f87a'
-
+ADb = b'ADb'
+yb = 'd2316b454718c35362d83d69df6320f38578ed5984651435e2949762d900b80d'
+Yb = '08bcf6e9777a9c313a3db6daa510f2d398403319c2341bd506a92e672eb7e307'
 
 s = '7cd0e075fa7955ba52c02759a6c90dbbfc10e6d40aea8d283e407d88cf538a05'
 Y_i1 = '2b3c6b8c4f3800e7aef6864025b4ed79bd599117e427c41bd47d93d654b4a51c'
@@ -26,7 +28,7 @@ class TestCpaceR255(unittest.TestCase):
         self.assertEqual(g, bytes.fromhex(generator))
 
     def test_compute_Ya(self):
-        cpace = CPace(PRS, ADa=ADa, CI=bytes.fromhex(CI), sid=bytes.fromhex(sid))
+        cpace = CPace(PRS, mo='oc', ADa=ADa, ADb=b'', CI=bytes.fromhex(CI), sid=bytes.fromhex(sid))
         Ya_star, ADa_star = cpace.compute_Ya()
         self.assertEqual(Ya_star, bytes.fromhex(Ya))
         self.assertEqual(ADa_star, ADa)
@@ -36,6 +38,30 @@ class TestCpaceR255(unittest.TestCase):
             scalar_mult_vfy(bytes.fromhex(s), bytes.fromhex(Y_i1))
         with self.assertRaises(ValueError):
             scalar_mult_vfy(bytes.fromhex(s), bytes.fromhex(Y_i2))
+
+    def test_lexiographically_larger(self):
+        self.assertFalse(lexiographically_larger(b"\0", b"\0\0"))
+        self.assertTrue(lexiographically_larger(b"\1", b"\0\0"))
+        self.assertTrue(lexiographically_larger(b"\0\0", b"\0"))
+        self.assertFalse(lexiographically_larger(b"\0\0", b"\1"))
+        self.assertFalse(lexiographically_larger(b"\0\1", b"\1"))
+        self.assertFalse(lexiographically_larger(b"ABCD", b"BCD"))
+
+    def test_o_cat(self):
+        self.assertEqual(o_cat(b"ABCD",b"BCD"), bytes.fromhex('6f6342434441424344'))
+        self.assertEqual(o_cat(b"BCD",b"ABCDE"), bytes.fromhex('6f634243444142434445'))
+
+    def test_transcript_oc(self):
+        self.assertEqual(transcript_oc(b"123", b"PartyA", b"234",b"PartyB"), bytes.fromhex('6f6303323334065061727479420331323306506172747941'))
+        self.assertEqual(transcript_oc(b"3456",b"PartyA",b"2345",b"PartyB"), bytes.fromhex('6f63043334353606506172747941043233343506506172747942'))
+
+    def test_transcript_ir(self):
+        self.assertEqual(transcript_ir(b"123", b"PartyA", b"234",b"PartyB"), bytes.fromhex('03313233065061727479410332333406506172747942'))
+        self.assertEqual(transcript_ir(b"3456",b"PartyA",b"2345",b"PartyB"), bytes.fromhex('043334353606506172747941043233343506506172747942'))
+
+    def test_K(self):
+        self.assertEqual(scalar_mult_vfy(bytes.fromhex(ya),bytes.fromhex(Yb)), bytes.fromhex('e22b1ef7788f661478f3cddd4c600774fc0f41e6b711569190ff88fa0e607e09'))
+        self.assertEqual(scalar_mult_vfy(bytes.fromhex(yb), bytes.fromhex(Ya)), bytes.fromhex('e22b1ef7788f661478f3cddd4c600774fc0f41e6b711569190ff88fa0e607e09'))
 
 if __name__ == '__main__':
     unittest.main()
